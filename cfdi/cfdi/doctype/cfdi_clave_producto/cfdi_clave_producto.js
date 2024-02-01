4a61f5af0cc2906a4f33a7ffbf089ed8df1fd7d6
ggarciangm <gilberto_garcia_del_rio@yahoo.com>
2024-02-01 03:17:33 +0000

    Alta de catalogos cfdi MX

---
+++ cfdi/cfdi/doctype/cfdi_clave_producto/cfdi_clave_producto.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, gdelriou and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('CFDI Clave Producto', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ cfdi/cfdi/doctype/regimen_fiscal/regimen_fiscal.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, gdelriou and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Regimen Fiscal', {
+	// refresh: function(frm) {
+
+	// }
+});
